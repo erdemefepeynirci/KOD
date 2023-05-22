b03bf7fd4b2ebcc2e04f2b4769d12c216b5a2914
EmineUyar <you@example.com>
2023-05-22 22:14:58 +0300

    c

--- Interactable.py
+++ Interactable.py
@@ -6,12 +6,17 @@ class Interactable:
         self.loc_x = loc_x
         self.loc_y = loc_y
 
-    def is_interacted(self):
-        pass
+    def is_interacted(self,locx,locy):
+        if locx==self.loc_x and locy==self.loc_y:
+            return True
+        else:
+            return False
+        
     def create(self,locx,locy,image,height,width):
         self.image = pygame.transform.scale(pygame.image.load(image),(height,width))
         self.loc_x = locx
         self.loc_y = locy
+        
     def draw(self,screen: pygame.Surface):
         screen.blit(self.image,(self.loc_x,self.loc_y))
 
