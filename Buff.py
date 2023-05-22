44ca9dcba6b00263fc8e82905b723d1f3ff37938
EmineUyar <you@example.com>
2023-05-23 00:18:46 +0300

    buffs created

--- Buff.py
+++ Buff.py
@@ -1,10 +1,11 @@
-import Interactable
+from Interactable import Interactable
 
 class Buff(Interactable):
     def __init__(self):
         self.effect_time: int
         self.remaining_time: int
         super().__init__(loc_x,loc_y)
+
 
     def create():
         pass
